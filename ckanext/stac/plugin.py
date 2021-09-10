fd34e631e48f56172b3bf9151d7105b5ceda70e2
Ben Weintraub <ben@Bens-MacBook-Pro.local>
2021-09-10 15:00:17 -0700

    add harvester gather and import stage functions

--- ckanext/stac/plugin.py
+++ ckanext/stac/plugin.py
@@ -2,6 +2,7 @@ from ckanext.harvest.harvesters.base import HarvesterBase
 from ckan.plugins.core import implements
 #from ckan.plugins.core import SingletonPlugin, implements
 from ckanext.harvest.interfaces import IHarvester
+import pystac
 
 class StacHarvester(HarvesterBase):
 #class StacHarvester(SingletonPlugin):
@@ -75,6 +76,90 @@ class StacHarvester(HarvesterBase):
         :param harvest_job: HarvestJob object
         :returns: A list of HarvestObject ids
         '''
+        """
+        def _request_datasets_from_socrata(domain, limit=100, offset=0):
+            api_request_url = \
+                '{0}?domains={1}&search_context={1}' \
+                '&only=datasets&limit={2}&offset={3}' \
+                .format(BASE_API_ENDPOINT, domain, limit, offset)
+            log.debug('Requesting {}'.format(api_request_url))
+            api_response = requests.get(api_request_url)
+
+            try:
+                api_json = api_response.json()
+            except JSONDecodeError:
+                self._save_gather_error(
+                    'Gather error: Invalid response from {}'
+                    .format(api_request_url),
+                    harvest_job)
+                return None
+
+            if 'error' in api_json:
+                self._save_gather_error('Gather error: {}'
+                                        .format(api_json['error']),
+                                        harvest_job)
+                return None
+
+            return api_json['results']
+
+        def _page_datasets(domain, batch_number):
+            '''Request datasets by page until an empty array is returned'''
+            current_offset = 0
+            while True:
+                datasets = \
+                    _request_datasets_from_socrata(domain, batch_number,
+                                                   current_offset)
+                if datasets is None or len(datasets) == 0:
+                    raise StopIteration
+                current_offset = current_offset + batch_number
+                for dataset in datasets:
+                    yield dataset
+        """
+        def _make_harvest_objs(datasets):
+            '''Create HarvestObject with STAC dataset content.'''
+            obj_ids = []
+            guids = []
+            for d in datasets:
+                log.debug('Creating HarvestObject for {} {}'
+                          .format(d['resource']['name'],
+                                  d['resource']['id']))
+                obj = HarvestObject(guid=d['resource']['id'],
+                                    job=harvest_job,
+                                    content=json.dumps(d),
+                                    extras=[HarvestObjectExtra(
+                                                        key='status',
+                                                        value='hi!')])
+                obj.save()
+                obj_ids.append(obj.id)
+                guids.append(d['resource']['id'])
+            return obj_ids, guids
+
+        log.debug('In StacHarvester gather_stage (%s)',
+                  harvest_job.source.url)
+
+        #domain = urlparse(harvest_job.source.url).hostname
+        # set and read the catalog
+        catalog_url = "https://storage.googleapis.com/cfo-public/catalog.json"
+        catalog = pystac.Catalog.from_file(catalog_url)
+        veg = catalog.get_child('vegetation')
+        items = veg.get_all_items()
+
+        all_data=[]
+        for item in veg.get_all_items():
+            d={'name':item.properties['metric'],'title':item.id,'date':item.get_datetime(),'spatial_extent':item.bbox,'url':item.assets[item.properties['metric']].get_absolute_href()}
+            all_data.append(d)
+        
+        object_ids, guids = _make_harvest_objs(all_data)
+        #object_ids, guids = _make_harvest_objs(_page_datasets(domain, 100))
+
+        # Check if some datasets need to be deleted
+        object_ids_to_delete = \
+            self._mark_datasets_for_deletion(guids, harvest_job)
+
+        object_ids.extend(object_ids_to_delete)
+
+        return object_ids
+
 
     def fetch_stage(self, harvest_object):
         '''
@@ -94,6 +179,8 @@ class StacHarvester(HarvesterBase):
         :returns: True if successful, 'unchanged' if nothing to import after
                 all, False if not successful
         '''
+        # No fetch required, all package data obtained from gather stage.
+        return True
 
     def import_stage(self, harvest_object):
         '''
@@ -121,3 +208,108 @@ class StacHarvester(HarvesterBase):
         :returns: True if the action was done, "unchanged" if the object didn't
                 need harvesting after all or False if there were errors.
         '''
+        log.debug('In StacHarvester import_stage')
+
+        base_context = {
+            'model': model,
+            'session': model.Session,
+            'user': self._get_user_name(),
+            'ignore_auth': True
+        }
+
+        status = self._get_object_extra(harvest_object, 'status')
+        if status == 'delete':
+            # Delete package
+            toolkit.get_action('package_delete')(
+                base_context, {'id': harvest_object.package_id})
+            log.info('Deleted package {0} with guid {1}'
+                     .format(harvest_object.package_id, harvest_object.guid))
+            return True
+
+        if not harvest_object:
+            log.error('No harvest object received')
+            return False
+
+        if harvest_object.content is None:
+            self._save_object_error('Empty content for object %s' %
+                                    harvest_object.id,
+                                    harvest_object, 'Import')
+            return False
+
+        # Get the last harvested object (if any)
+        previous_object = model.Session.query(HarvestObject) \
+            .filter(HarvestObject.guid == harvest_object.guid) \
+            .filter(HarvestObject.current == True) \
+            .first()  # noqa
+
+        # Flag previous object as not current anymore
+        if previous_object:
+            previous_object.current = False
+            previous_object.add()
+
+        # Flag this object as the current one
+        harvest_object.current = True
+        harvest_object.add()
+
+        # Check if a dataset with the same guid exists
+        existing_dataset = self._get_existing_dataset(harvest_object.guid)
+
+        # Delete package (dev testing)
+        # if existing_dataset:
+        #     self._delete_dataset(existing_dataset['id'])
+        # return False
+
+        package_dict = self._build_package_dict(base_context, harvest_object)
+
+        self.process_package(package_dict, harvest_object)
+
+        if existing_dataset:
+            # Do we need to update?
+            existing_updated_at = self._get_package_extra(existing_dataset,
+                                                          'source_updated_at')
+            source_updated_at = self._get_package_extra(package_dict,
+                                                        'source_updated_at')
+            if existing_updated_at and source_updated_at and \
+               parse(existing_updated_at) == parse(source_updated_at):
+                return 'unchanged'
+
+            package_dict['id'] = existing_dataset['id']
+            harvest_object.package_id = package_dict['id']
+            harvest_object.add()
+            try:
+                toolkit.get_action('package_update')(
+                    base_context.copy(),
+                    package_dict
+                )
+            except Exception as e:
+                self._save_object_error('Error updating package for {}: {}'
+                                        .format(harvest_object.id, e),
+                                        harvest_object, 'Import')
+                return False
+
+        else:
+            # We need to explicitly provide a package ID
+            package_dict['id'] = unicode(uuid.uuid4())
+
+            harvest_object.package_id = package_dict['id']
+            harvest_object.add()
+
+            # Defer constraints and flush so the dataset can be indexed with
+            # the harvest object id (on the after_show hook from the harvester
+            # plugin)
+            model.Session.execute(
+                'SET CONSTRAINTS harvest_object_package_id_fkey DEFERRED')
+            model.Session.flush()
+
+            try:
+                toolkit.get_action('package_create')(
+                    base_context.copy(),
+                    package_dict
+                )
+            except Exception as e:
+                self._save_object_error('Error creating package for {}: {}'
+                                        .format(harvest_object.id, e),
+                                        harvest_object, 'Import')
+                return False
+
+        return True
