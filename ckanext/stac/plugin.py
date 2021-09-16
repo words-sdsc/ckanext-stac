b8093244b2532f87e7cfa5786b922c22fb155ae7
Ben Weintraub <ben@Bens-MacBook-Pro.local>
2021-09-16 10:47:41 -0700

    add random id

--- ckanext/stac/plugin.py
+++ ckanext/stac/plugin.py
@@ -14,6 +14,7 @@ import requests, json
 from ckan.lib.munge import munge_title_to_name, munge_tag
 from urlparse import urlparse
 import logging
+import random
 log = logging.getLogger(__name__)
 
 class StacHarvester(HarvesterBase):
@@ -363,7 +364,7 @@ class StacHarvester(HarvesterBase):
                             if data['properties']['metric']==dataset:
                                 resources.append({'name':data['id'],'title':data['id'],'description':data['description'],'url':data['assets'][dataset]['href'],'format':'tiff'})  
                     
-                        dataJSON.append({'name':dataset.lower(), 'id':dataset.lower(), 'title':dataset,'notes' :data['description'],'license_id':data['license'],'url':"https://storage.googleapis.com/cfo-public/catalog.json",'extras': [{'key':'spatial extent','value':str(data['extent']['spatial']['bbox'])},
+                        dataJSON.append({'name':dataset.lower(), 'id':random.randint(10000000,1000000000), 'title':dataset,'notes' :data['description'],'license_id':data['license'],'url':"https://storage.googleapis.com/cfo-public/catalog.json",'extras': [{'key':'spatial extent','value':str(data['extent']['spatial']['bbox'])},
                         {'key':'temporal extent','value':str(data['extent']['temporal']['interval'])}],'resources':resources})
 
             return dataJSON
