430f99762fc37e28911d0cb4519df1d25948b887
Ben Weintraub <ben@Bens-MacBook-Pro.local>
2021-09-17 09:35:50 -0700

    access domain from ckan gui

--- ckanext/stac/plugin.py
+++ ckanext/stac/plugin.py
@@ -405,8 +405,8 @@ class StacHarvester(HarvesterBase):
         log.debug('In StacHarvester gather_stage (%s)',
                   harvest_job.source.url)
 
-        #domain = urlparse(harvest_job.source.url).hostname
-        domain = 'https://storage.googleapis.com/cfo-public/vegetation/collection.json'
+        domain = urlparse(harvest_job.source.url).hostname
+        #domain = 'https://storage.googleapis.com/cfo-public/vegetation/collection.json'
 
         """  
         all_data = [{
